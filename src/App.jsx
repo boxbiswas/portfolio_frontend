2f0b598a8413d396c6ad1c9a54f7e35e2911b3c6
INDRASISH BISWAS <indrasish.biswas2006@gmail.com>
2026-09-25 22:16:04 +0530

    DAY 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,12 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import Dashboard from './pages/Dashboard';
-import HistoryPage from './pages/HistoryPage';
-import ProtectedRoute from './components/ProtectedRoute';
-import AppLayout from './components/layout/AppLayout';
-
 function App() {
-  const { isAuthenticated } = useSelector((state) => state.auth);
 
   return (
     <BrowserRouter>
-      <Toaster position="top-center" />
       <Routes>
-        {/* Auth Routes - Redirect to app if already logged in */}
-        <Route path="/login" element={isAuthenticated ? <Navigate to="/app" replace /> : <LoginPage />} />
-        <Route path="/register" element={isAuthenticated ? <Navigate to="/app" replace /> : <RegisterPage />} />
-        
-        {/* Protected Routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route element={<AppLayout />}>
-            <Route path="/app" element={<Dashboard />} />
-            <Route path="/history" element={<HistoryPage />} />
-          </Route>
-        </Route>
-        
-        {/* Redirect unknown routes */}
-        <Route path="*" element={<Navigate to="/app" replace />} />
+
       </Routes>
     </BrowserRouter>
   );
